3497385c4c826dfe990a540d56656e941b622969
Leo_m <l.marque69210@gmail.com>
2024-03-04 10:29:28 +0100

    ajout navbar et formulaire d'inscription

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="media/css/index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css" integrity="sha384-BY+fdrpOd3gfeRvTSMT+VUZmA728cfF9Z2G42xpaRkUGu2i3DyzpTURDo5A6CaLK" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Les retards de la SNCF</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Les Retards SNCF</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#">Les derniers retards</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" href="inscription.php">Inscription</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#">Connexion</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <p>Bienvenue sur notre site dédié aux retards des trains de la SNCF.</p>
+        <p>Ici, vous pourrez trouver des informations sur les retards actuels, les causes probables, et bien plus encore.</p>
+        <p>N'hésitez pas à naviguer à travers les différentes sections pour obtenir les dernières mises à jour.</p>
+    </main>
+    <footer>
+        <p>&copy; 2024 Les retards de la SNCF. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
